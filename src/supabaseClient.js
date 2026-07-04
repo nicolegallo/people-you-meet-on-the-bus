fc1f948afd5f7172462b2529ed07d358c15034af
Nicole Gallo <40307517+nicolegallo@users.noreply.github.com>
2026-07-03 20:05:47 -0400

    Use environment variables for Supabase client config

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://irvfzqzmhybmqgqnlnxx.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlydmZ6cXptaHlibXFncW5sbnh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwOTg2NTMsImV4cCI6MjA5ODY3NDY1M30.aFepTruegpyV-RWQPFKUuMIXOfE3W8XjXmMYZp1e9lU'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
